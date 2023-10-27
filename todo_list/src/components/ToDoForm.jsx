0a913ff28400f4936f2b38c016f73c0b0ff4de25
Satyum <satyumspathania@gmail.com>
2023-10-27 11:05:19 +0530

    setting up props and adding todos

--- todo_list/src/components/ToDoForm.jsx
+++ todo_list/src/components/ToDoForm.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const todoform = () => {
+export const ToDoForm = ({ addTodo }) => {
+
+    const [value, setvalue] = useState("");
+
+    const handleChange = (e) => {
+        setvalue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setvalue("");
+    }
+
     return (
-        <div>
-
-        </div>
+        <form className="TodoForm" onSubmit={handleSubmit}>
+            <input type="text" value={value} className='todo-input' placeholder="Task for Today" onChange={handleChange} />
+            <button type="submit" className='todo-btn'>Add Task</button>
+        </form>
     )
 }
